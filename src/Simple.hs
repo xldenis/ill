2ac3cfc88e031059b85aafa24f4691043f5da3a8
Xavier Denis <xldenis@gmail.com>
2016-04-02 20:03:12 -0500

    Basic substitution for HM types

--- src/Simple.hs
+++ src/Simple.hs
@@ -1,32 +1,58 @@
-{-# LANGUAGE MultiParamTypeClasses, DeriveGeneric, DeriveDataTypeable #-}
 module Simple where
-import Control.Applicative
-import Control.Lens
-import GHC.Generics
-import Data.Data
-import Data.Data.Lens
+  import Control.Lens
+  import Control.Lens.Setter
+  import Data.List (union, intersect)
+  import Control.Monad
 
-data Expr = Var Int | Pos Expr String | Neg Expr | Add Expr Expr deriving (Eq,Ord,Show,Read,Generic,Data,Typeable)
-data Stmt = Seq [Stmt] | Sel [Expr] | Let String Expr deriving (Eq,Ord,Show,Read,Generic,Data,Typeable)
+  data Kind = Star | KFn Kind Kind
+    deriving (Show, Eq)
 
-instance Plated Expr where
-  plate f (Var x  ) = pure (Var x)
-  plate f (Pos x y) = Pos <$> f x <*> pure y
-  plate f (Neg x  ) = Neg <$> f x
-  plate f (Add x y) = Add <$> f x <*> f y
+  type Id = String
 
-instance Plated Stmt where
-  plate f (Seq xs) = Seq <$> traverse f xs
-  plate f (Sel xs) = pure (Sel xs)
-  plate f (Let x y) = pure (Let x y)
+  data Type = TVar Tyvar | TAp Type Type | TCon Tycon | TGen Int deriving (Show, Eq)
 
-exprs :: Traversal' Stmt Expr
-exprs f (Seq xs)  = Seq <$> traverse (exprs f) xs
-exprs f (Sel xs)  = Sel <$> traverse f xs
-exprs f (Let x y) = Let x <$> f y
+  data Tyvar = Tyvar Id Kind deriving (Show, Eq)
+
+  data Tycon = Tycon Id Kind deriving (Show, Eq)
+
+  class HasKind k where
+    kind :: k -> Kind
+
+  instance HasKind Tyvar where
+    kind (Tyvar _ k) = k
+
+  instance HasKind Tycon where
+    kind (Tycon _ k) = k
+
+  instance HasKind Type where
+    kind (TVar t) = kind t
+    kind (TAp l r) = case kind l of -- l :: k -> k', r :: k => l (r) :: k'
+      (KFn k k') -> k'
+    kind (TCon c) = kind c
+
+  type Substitution = [(Tyvar, Type)]
+
+  class Types a where
+    apply :: Substitution -> a -> a
+    free  :: a -> [Tyvar]
+
+  instance Types Type where
+    apply sbst free@(TVar t) = case lookup t sbst of
+      Just tp -> tp
+      Nothing -> free
+    apply sbst (TAp l r) = TAp (apply sbst l) (apply sbst r)
+
+    free (TVar t)  = [t]
+    free (TAp l r) =  free l `union` free r
+    free t         = []
+
+  compose :: Substitution -> Substitution -> Substitution
+  compose s1 s2 = over (traverse._2) (apply s1) s2 ++ s1
 
 
-ast = Seq [Sel [(Var 1) `Add` (Var 2), Pos (Var 1) "AAAa"]]
+  -- Make MonadFail when GHC 8 drops
+  merge :: Monad m => Substitution -> Substitution -> m Substitution
+  merge s1 s2 = if agree then return (s1 ++ s2) else fail "substitution merge failure"
+    where agree = all (\x -> apply s1 x == apply s2 x) (map fst s1 `intersect` map fst s2)
 
-rew (Let _ _) = Nothing
-rew a = Just a
+
