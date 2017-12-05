c077a9d31e3a5af92bfb0540b6d1e31406f6c0fd
Xavier Denis <xldenis@gmail.com>
2017-12-04 23:06:36 -0600

    Continue prototyping code gen

---
+++ docs/codegen-notes.md
@@ -0,0 +1,13 @@
+Challenges with code generation
+
+- lexical closures:
+  - closure conversion
+  - lambda lifting
+  - unclear which is better / more modern
+- polymorphic functions
+  - template instantiation?
+  - what else?
+- polymorphic types
+  - how to represent polymorphism in llvm ir?
+    - boxing!
+    https://www.youtube.com/watch?v=ctS8FzqcRug
--- src/Ill/Codegen.hs
+++ src/Ill/Codegen.hs
@@ -3,36 +3,62 @@ module Ill.Codegen where
 
 import Ill.Syntax.Core
 import Ill.Syntax.Type
+import Ill.Syntax.Name
 
 import IRBuilder.Monad
 import IRBuilder.Instruction
-import LLVM.AST.Type as AST
-import LLVM.AST.Name
+import qualified LLVM.AST.Type as T
+import LLVM.AST.AddrSpace
+
 import LLVM.Pretty
 
 import Data.String
 import ModuleBuilder
 
 import Control.Monad (forM)
+import qualified Control.Monad as M
 
 import Ill.Prelude hiding (void)
 
 prettyModule mod = ppllvm $ compileModule mod
 
 compileModule mod = buildModule "example" $ mdo
-  forM (bindings mod) compileBinding
+  forM (constructors mod) compileConstructor
+  forM (filter isLambda $ bindings mod) compileBinding
+  where
+  isLambda (NonRec _ Lambda{}) = True
+  isLambda _ = False
 
-compileBinding (NonRec nm l@(Lambda _ _)) = function (Name . fromString $ varName nm) args ret $ \args -> mdo
+compileConstructor :: MonadModuleBuilder m => (Name, (Int, Type Name)) -> m ()
+compileConstructor (nm, (_, ty)) =
+  typedef (fromString nm) (Just $ T.StructureType False llvmArgTys)
+  where
+  llvmArgTys = map typeToLlvmType argTys
+  argTys = init (unwrapFnType ty)
+
+ptr x = T.PointerType x (AddrSpace 0)
+typeToLlvmType = ptr . typeToLlvmType'
+typeToLlvmType' (TVar nm) = T.NamedTypeReference (fromString nm)
+typeToLlvmType' (Arrow a b) = T.FunctionType (typeToLlvmType' b) [typeToLlvmType' a] False
+typeToLlvmType' (TAp (TAp (TConstructor "->") a) b) = T.FunctionType (typeToLlvmType' b) [typeToLlvmType' a] False
+typeToLlvmType' (TConstructor nm) = T.NamedTypeReference (fromString nm)
+typeToLlvmType' t = T.void
+
+
+compileBinding :: MonadModuleBuilder m => Bind Var -> m ()
+compileBinding (NonRec nm l@(Lambda _ _)) = M.void . function (fromString $ varName nm) args ret $ \args -> mdo
   block `named` "entry" ; do
     unreachable
-
   pure ()
+
   where
-  args = []
-  ret  = void
+  args = map (\var -> (typeToLlvmType (idTy var), fromString $ varName var)) argVars
+  ret  = T.void
+  (body, argVars) = unwrapLambda l
   unwrapLambda :: Core Var -> (Core Var, [Var])
   unwrapLambda (Lambda b@(Id{}) e) = (b :) <$> unwrapLambda e
   unwrapLambda (Lambda _ e) = unwrapLambda e
   unwrapLambda e = (e, [])
+-- compileBinding exp = unreachable -- error $ show exp
 
 compileBody exp = error $ show exp
