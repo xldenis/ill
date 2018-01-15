b394338d7230b58264d8c19cf8cce3a582757d30
Xavier Denis <xldenis@gmail.com>
2018-01-15 11:29:26 -0600

    Work on generating code for calls

--- src/Ill/Codegen.hs
+++ src/Ill/Codegen.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE RecursiveDo, OverloadedStrings #-}
+{-# LANGUAGE RecursiveDo, OverloadedStrings, ConstraintKinds #-}
 module Ill.Codegen where
 
 import Control.Monad (forM)
@@ -26,14 +26,17 @@ import           Ill.Prelude hiding (void)
 import           Data.Word
 import           Data.Maybe
 import           Data.List (find)
-import Control.Monad.Fix
+import           Control.Monad.Fix
+import           Control.Monad.Reader
 
 import Debug.Trace
+
+import Data.Text.Lazy (pack)
 
 {-
   init doesnt work
 -}
-prettyModule mod = ppllvm $ compileModule mod
+prettyModule mod =  ppllvm $ compileModule mod
 
 insertvalue :: MonadIRBuilder m => Operand -> Operand -> [Word32] -> m Operand
 insertvalue agg el is = emitInstr (T.typeOf agg) $ AST.InsertValue agg el is []
@@ -45,8 +48,20 @@ malloc :: MonadIRBuilder m => AST.Operand -> m AST.Operand
 malloc size = call (AST.LocalReference mallocTy "malloc") [(size, [])]
   where mallocTy = T.FunctionType (ptr T.void) [T.i32] False
 
-compileModule mod = buildModule "example" $ mdo
+data CodegenState = CS
+  { vars :: [(Id, AST.Operand)]
+  , arities :: [(Id, Int)]
+  } deriving (Show, Eq)
+
+updateVars f (CS v a) = CS (f v) a
+updateArities f (CS v a) = CS v (f a)
+
+compileModule :: CoreModule -> AST.Module
+compileModule mod =  buildModule "example" . (flip runReaderT (CS nameMap arityMap)) $ mdo
   extern "malloc" [T.NamedTypeReference "size_t"] (ptr T.void)
+
+  defMkDouble
+  defMkInt
 
   forM (constructors mod) compileConstructor
   forM (filter isLambda $ bindings mod) compileBinding
@@ -54,12 +69,35 @@ compileModule mod = buildModule "example" $ mdo
   isLambda (NonRec _ Lambda{}) = True
   isLambda _ = False
 
+  nameMap = map nameTuple (bindings mod) ++ map (\(nm, (_, t)) -> (nm, ConstantOperand $ C.GlobalReference (typeToLlvmType' t) (fromString nm))) (constructors mod)
+  nameTuple (NonRec v _) = (varName v, ConstantOperand $ C.GlobalReference (typeToLlvmType' (idTy v)) (fromString $ varName v))
+  arityMap = map arityTuple (bindings mod) ++ map (\(nm, (i, _)) -> (nm, i)) (constructors mod)
+  arityTuple (NonRec v b) = (varName v, length . snd $ unwrapLambda b)
+
+defMkDouble = function "mkDouble" [(T.double, "d")] (ptr T.void) $ \[d] -> do
+  block `named` "entry" ; do
+    memPtr <- malloc =<< (int64 $ 8 + 8)
+    store memPtr 8 d
+    ret memPtr
+
+mkDouble d = call (AST.LocalReference mkDoubleTy "mkDouble") [(d, [])]
+  where mkDoubleTy = T.FunctionType (ptr T.double) [T.double] False
+
+defMkInt = function "mkInt" [(T.i64, "d")] (ptr $ T.NamedTypeReference (fromString "Int")) $ \[d] -> do
+  block `named` "entry" ; do
+    memPtr <- malloc =<< (int64 $ 8 + 8)
+    store memPtr 8 d
+    ret memPtr
+
+mkInt d = call (AST.ConstantOperand $ C.GlobalReference mkIntTy "mkInt") [(d, [])]
+  where mkIntTy = T.FunctionType (ptr $ T.NamedTypeReference (fromString "Int")) [T.i64] False
+
 compileConstructor :: MonadModuleBuilder m => (Name, (Int, Type Name)) -> m ()
 compileConstructor (nm, (_, ty)) = do
   typedef (fromString nm) (Just $ T.StructureType False llvmArgTys)
 
   M.void $ function (fromString $ nm) funArgs (ptr consTy) $ \args -> do
-    block `named` "entry" ; do
+    block `named` "entryC" ; do
       voidPtr <- malloc (ConstantOperand consSize)
       memPtr <- bitcast voidPtr (ptr consTy)
       val <- load memPtr 8
@@ -92,47 +130,64 @@ typeToLlvmType' ap@(TAp _ _) = let
 typeToLlvmType' (Forall _ t) = typeToLlvmType' t
 typeToLlvmType' t = error $ show t
 
-compileBinding :: (MonadFix m, MonadModuleBuilder m) => Bind Var -> m ()
+type CodegenM m = (MonadFix m, MonadIRBuilder m, MonadReader CodegenState m)
+type ModuleM m = (MonadFix m, MonadModuleBuilder m, MonadReader CodegenState m)
+
+compileBinding :: ModuleM m => Bind Var -> m ()
 compileBinding (NonRec nm l@(Lambda _ _)) = M.void . function (fromString $ varName nm) args retTy $ \args -> mdo
   block `named` "entry" ; do
     let dict = zipWith (\v op -> (fromString $ varName v, op)) argVars args
-    compileBody dict body >>= ret
+    local (updateVars (dict ++)) $ compileBody body >>= ret
   pure ()
 
   where
   args = map (\var -> (typeToLlvmType (idTy var), fromString $ varName var)) argVars
   retTy = typeToLlvmType $ last $ unwrapFnType $ idTy nm
   (body, argVars) = unwrapLambda l
-  unwrapLambda :: Core Var -> (Core Var, [Var])
-  unwrapLambda (Lambda b@(Id{}) e) = (b :) <$> unwrapLambda e
-  unwrapLambda (Lambda _ e) = unwrapLambda e
-  unwrapLambda e = (e, [])
+
+unwrapLambda :: Core Var -> (Core Var, [Var])
+unwrapLambda (Lambda b@(Id{}) e) = (b :) <$> unwrapLambda e
+unwrapLambda (Lambda _ e) = unwrapLambda e
+unwrapLambda e = (e, [])
 -- compileBinding exp = unreachable -- error $ show exp
 
-compileBody :: (MonadFix m, MonadIRBuilder m) => [(Id, AST.Operand)] -> CoreExp -> m AST.Operand
-compileBody dict (Lit (Double d)) = double d
-compileBody dict (Lit (Integer i)) = int64 i
-compileBody dict (Var v) = pure . fromJust $ varName v `lookup` dict
-compileBody dict (Let (NonRec v e) exp) = do -- figure out how to handle recursive let bindings
-  cE <- compileBody dict e
+compileBody :: CodegenM m => CoreExp -> m AST.Operand
+compileBody (Lit (Double d)) = mkDouble =<< double d -- BOX IT
+compileBody (Lit (Integer i)) = mkInt =<< int64 i
+compileBody (Var v) = do
+  dict <- reader vars
+  pure . nameAssert $ varName v `lookup` dict
+  where
+  nameAssert (Just nm) = nm
+  nameAssert Nothing = error $ "no " ++ varName v ++ " found."
+compileBody (Let (NonRec v e) exp) = do -- figure out how to handle recursive let bindings
+  cE <- compileBody e
 
-  compileBody ((varName v, cE) : dict) exp
-compileBody dict (Case scrut alts) = mdo
-  scrutOp <- compileBody dict scrut
+  local (updateVars ((varName v, cE) :)) $ compileBody exp
+compileBody (Case scrut alts) = mdo
+  scrutOp <- compileBody scrut
   val <- load scrutOp 8
   tag <- extractvalue (T.i64) val [0]
 
   switch tag defAlt alts'
 
   (alts', phis) <- unzip <$> M.zipWithM (compileAlt retBlock val) [1..] (filter isConAlt alts)
-  defAlt <- (compileDefaultAlt retBlock) (fromJust $ find isTrivialAlt alts)
+  defAlt <- fromJust $ (compileDefaultAlt retBlock) <$> (find isTrivialAlt alts) <|> pure (defaultBranch retBlock)
 
   retBlock <- block `named` "switch_return" ; do
     phi phis
   where
-  compileDefaultAlt retB (TrivialAlt b) = do
+  fromJust' (Just x) = x
+  fromJust' _ = error (show alts)
+
+  defaultBranch retB = do
     blk <- block `named` "default_branch" ; do
-      compileBody dict b
+      br retB
+    pure blk
+
+  compileDefaultAlt retB (TrivialAlt b) = do
+    blk <- block `named` "trivial_branch" ; do
+      compileBody b
       br retB
     pure blk
 
@@ -145,11 +200,37 @@ compileBody dict (Case scrut alts) = mdo
         NotUsed -> pure Nothing)
 
     let bindDict = zipWith (\var val -> (fromString $ varName var, val)) binds bindVals
-    compileBody (bindDict ++ dict) b
+    let tag = C.Int 64 i
+
+    local (updateVars (bindDict ++)) $ compileBody b
     val <- int64 5
     br retB
-    let tag = C.Int 64 i
     pure ((tag, block), (val, block))
-    where
 
-compileBody _ exp = int64 1
+compileBody a@(App _ _) = compileCall (unwrapApp a [])
+compileBody exp = mkInt =<< int64 (-97)
+
+unwrapApp :: Core Var -> [CoreExp] -> (Core Var, [CoreExp])
+unwrapApp (App f (Type _)) acc = unwrapApp f (acc)
+unwrapApp (App f e)        acc = unwrapApp f (e : acc)
+unwrapApp e acc = (e, acc)
+
+compileCall (c, []) = compileBody c
+compileCall (Var func, args) = do
+  globalDefs <- reader arities
+
+  case varName func `lookup` globalDefs of
+    Just arity | arity == length args -> knownCall arity func args
+               | arity <  length args -> mkInt =<< int64 (-98) -- take first arity args and then do uknnown call
+               | arity >  length args -> mkInt =<< int64 (-99) -- mkClosure arity func args
+    Nothing -> mkInt =<< int64 (-96) -- unknownCall func args
+
+knownCall arity i@(Id{}) args = do
+  args' <- mapM compileBody args
+  f' <- compileBody (Var i)
+  traceShowM (f')
+  call f' (map (\arg -> (arg, [])) args')
+
+unknownCall = undefined
+
+mkClosure = undefined
